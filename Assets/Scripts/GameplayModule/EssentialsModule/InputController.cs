e4c47c3e987e88262984aee3a4ae2b979b5f7da8
Daniel <d.pardo@getaclub.io>
2022-01-30 14:13:26 -0500

    Implemented Notes

---
+++ Assets/Scripts/GameplayModule/EssentialsModule/InputController.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+
+namespace EmotionQuest.InputModule
+{
+    public class InputController : MonoBehaviour
+    {
+        #region ----Fields----
+        [SerializeField] private KeyCode growHappinessKey;
+        [SerializeField] private KeyCode growSadnessKey;
+        [SerializeField, Range(.1f, 1f)] private float timeForContinuousInput;
+        [SerializeField, Range(.01f, .1f)] private float timeBetweenContinuosInputs;
+
+        public Action growHappiness;
+        public Action growSadness;
+
+        private float timer = 0;
+        #endregion ----Fields----
+
+        #region ----Methods----
+        void Update()
+        {
+            if (!CheckInput(growHappinessKey, () => growHappiness?.Invoke()))
+                return;
+
+            CheckInput(growSadnessKey, () => growSadness?.Invoke());
+        }
+
+        public bool continousInput;
+        private bool CheckInput(KeyCode keycode, Action callback)
+        {
+            if (Input.GetKeyDown(keycode))
+            {
+                callback?.Invoke();
+                return false;
+            }
+            else if (Input.GetKey(keycode))
+            {
+                float timeUsing = continousInput ? timeBetweenContinuosInputs : timeForContinuousInput;
+                if (timer < timeUsing)
+                {
+                    timer += Time.deltaTime;
+                    return false;
+                }
+
+                timer = 0;
+                if (!continousInput)
+                    continousInput = true;
+                callback?.Invoke();
+                return false;
+            }
+            else if (Input.GetKeyUp(keycode))
+            {
+                continousInput = false;
+                timer = 0;
+            }
+            return true;
+        }
+        #endregion ----Methods----
+    }
+}
---
+++ Assets/Scripts/GameplayModule/OrbModule/Model/OrbData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace EmotionQuest.GameplayModule.OrbModule
+{
+    [System.Serializable]
+    public struct OrbData
+    {
+        public TypeOfOrb typeOfOrb;
+        public SizeOfOrb sizeOfOrb;
+
+        public bool CompareOrbData(OrbData other)
+        {
+            if (other.typeOfOrb != this.typeOfOrb)
+                return false;
+            if (other.sizeOfOrb != this.sizeOfOrb)
+                return false;
+            return true;
+        }
+    }
+}

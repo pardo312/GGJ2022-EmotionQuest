31ffff7a1c3b52f8ce27679fd49581f0d6cbb5d3
JesusCarcamoJC <jesus.carcamo@jamcity.com>
2022-10-17 19:20:12 -0500

    Fix notes velocity change on differents screen resolution to be synchronized with the music, without this the notes moves faster on smaller canvases than the reference resolution canvas, or slower in bigger canvases

--- Assets/Scripts/GameplayModule/OrbModule/Logic/Notes/NotesManager.cs
+++ Assets/Scripts/GameplayModule/OrbModule/Logic/Notes/NotesManager.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace EmotionQuest.GameplayModule.OrbModule
 {
     public class NotesManager : MonoBehaviour
     {
         [SerializeField] private float trackSpeed = 15;
+
+        private CanvasScaler canvasScaler;
         private bool isPlaying = false;
 
         public void Init() =>
             isPlaying = true;
 
+        private void Awake() =>
+            canvasScaler = GetComponentInParent<CanvasScaler>();
+
         private void Update()
         {
             if(isPlaying)
-                transform.position -= new Vector3(trackSpeed * 100 * Time.deltaTime, 0, 0);
+                transform.position -= new Vector3(trackSpeed * 100 * Time.deltaTime * Screen.width / canvasScaler.referenceResolution.x, 0, 0);
         }
 
         public void EndGame() =>
